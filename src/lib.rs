1d85c443d38ad198c90cb641fb9297ae8403d0c0
Gabriel Marcano <gabemarcano@yahoo.com>
2024-05-20 17:34:38 -0700

    Add docstring to lib.rs
     - Apparently when both lib.rs and main.rs exist, lib.rs takes
       precedence when it comes to documentation. As both the binary and
       library crates have the same name, cargo doc doesn't want to produce
       documentation for both.

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,15 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
+//! This crate contains traits for interfacing with Sparkfun Variable Loader (SVL) found on
+//! Sparkfun's Artemis modules. This tool is derived from Sparkfun's
+//! [svl.py](https://github.com/sparkfun/Apollo3_Uploader_SVL) implementation, and extended with a
+//! few extra commands.
+//!
+//! A binary companion crate is provided that is compatible with the original SVL, and is able to
+//! run faster, read memory, and use a GPIO to determine whether or not to skip the bootloader
+//! completely using an [enhanced bootloader
+//! version](https://github.com/gemarcano/svl.git).
+
 pub mod svl;
 pub use svl::*;
